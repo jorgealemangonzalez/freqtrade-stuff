65485a6867f5865693cc52dfa99842aa6c0e07ce
BlueTrading <62881979+BlueTrading@users.noreply.github.com>
2021-09-23 03:47:40 +0200

    Update Semaphore_1776_v2_4h_ema20_UP_DOWN.py

--- dry-run/03/Semaphore_1776_v2_4h_ema20_UP_DOWN.py
+++ dry-run/03/Semaphore_1776_v2_4h_ema20_UP_DOWN.py
@@ -74,57 +74,6 @@ class Semaphore_1776_v2_4h_ema20_UP_DOWN(IStrategy):
         dataframe1h['hma67'] = ftt.hull_moving_average(dataframe1h, 67)
         dataframe1h['hma40'] = ftt.hull_moving_average(dataframe1h, 40)
 
-    def pivots_points(dataframe: pd.DataFrame, timeperiod=60, levels=1) -> pd.DataFrame:
-    """
-    Pivots Points
-    https://www.tradingview.com/support/solutions/43000521824-pivot-points-standard/
-    Formula:
-    Pivot = (Previous High + Previous Low + Previous Close)/3
-    Resistance #1 = (2 x Pivot) - Previous Low
-    Support #1 = (2 x Pivot) - Previous High
-    Resistance #2 = (Pivot - Support #1) + Resistance #1
-    Support #2 = Pivot - (Resistance #1 - Support #1)
-    Resistance #3 = (Pivot - Support #2) + Resistance #2
-    Support #3 = Pivot - (Resistance #2 - Support #2)
-    ...
-    :param dataframe:
-    :param timeperiod: Period to compare (in ticker)
-    :param levels: Num of support/resistance desired
-    :return: dataframe
-    """
-
-    data = {}
-
-    low = qtpylib.rolling_mean(
-        series=pd.Series(index=dataframe.index, data=dataframe["low"]), window=timeperiod
-    )
-
-    high = qtpylib.rolling_mean(
-        series=pd.Series(index=dataframe.index, data=dataframe["high"]), window=timeperiod
-    )
-
-    # Pivot
-    data["pivot"] = qtpylib.rolling_mean(series=qtpylib.typical_price(dataframe), window=timeperiod)
-
-    # Resistance #1
-    data["r1"] = (2 * data["pivot"]) - low
-
-    # Resistance #2
-    data["s1"] = (2 * data["pivot"]) - high
-
-    # Calculate Resistances and Supports >1
-    for i in range(2, levels + 1):
-        prev_support = data["s" + str(i - 1)]
-        prev_resistance = data["r" + str(i - 1)]
-
-        # Resitance
-        data["r" + str(i)] = (data["pivot"] - prev_support) + prev_resistance
-
-        # Support
-        data["s" + str(i)] = data["pivot"] - (prev_resistance - prev_support)
-
-    return pd.DataFrame(index=dataframe.index, data=data)
-
             # MACD
         macd = ta.MACD(dataframe1h, fastperiod=12,
                        slowperiod=26, signalperiod=9)
